3170a92dcf5d16a9d9775c289fcfc4f3ec72e8dd
Michael Aerni <michael.aerni@hotmail.com>
2020-03-26 00:13:27 +0100

    Created migrations for the score model

---
+++ Migrations/20200325214123_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoschBot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserScores",
+                columns: table => new
+                {
+                    UserID = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Score = table.Column<ulong>(nullable: false),
+                    LastDailyClaimed = table.Column<DateTime>(nullable: true),
+                    CurrentDailyStreak = table.Column<ulong>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserScores", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserScores");
+        }
+    }
+}
